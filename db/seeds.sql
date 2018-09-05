c6d26b521b09d8aa3905d4379115227637c0ceed
FM PC <38448166+c-rter@users.noreply.github.com>
2018-09-05 11:27:15 -0400

    workingmodel

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hot Jalapeno Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cool Ranch Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
